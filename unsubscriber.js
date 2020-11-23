57fbc9d1c7be7ffe85a7ae46d9b3c188368c926e
Yurok Ji <49470364+yurokji@users.noreply.github.com>
2020-11-23 14:30:09 +0900

    Create unsubscriber.js

---
+++ unsubscriber.js
@@ -0,0 +1,35 @@
+function youtubeUnsubscriber() {
+    var count = document.querySelectorAll("ytd-channel-renderer:not(.ytd-item-section-renderer)").length;
+    var randomDelay = 500;
+
+    if(count == 0) return false;
+
+    function unsubscribeVisible(randomDelay) {
+
+        if (count == 0) {
+            window.scrollTo(0,document.body.scrollHeight);
+            setTimeout(function() {
+                youtubeUnsubscriber();
+            }, 10000)
+        }
+
+        unsubscribeButton = document.querySelector('.ytd-subscribe-button-renderer');
+        unsubscribeButton.click();
+
+        setTimeout(function () {
+            document.getElementById("confirm-button").click()
+            count--;
+            console.log("Remaining: ", count);
+
+            setTimeout(function () {
+                unsubscribedElement = document.querySelector("ytd-channel-renderer");
+                unsubscribedElement.parentNode.removeChild(unsubscribedElement);
+                unsubscribeVisible(randomDelay)
+            }, randomDelay);
+        }, randomDelay);
+    }
+
+    unsubscribeVisible(randomDelay);
+}
+
+youtubeUnsubscriber();
